a347d190d9e76bb3d2798ff6a212305b268ff74b
DarkStarsGaming <59534560+DarkStarsGaming@users.noreply.github.com>
2023-01-23 17:51:02 +0100

    Create redeemcodes.lua

---
+++ redeemcodes.lua
@@ -0,0 +1,68 @@
+local args = {
+    [1] = "Codes",
+    [2] = "Redeem",
+    [3] = {
+        ["Code"] = "140KAGAIN"
+    }
+}
+
+game:GetService("ReplicatedStorage").Replicator:InvokeServer(unpack(args))
+local args = {
+    [1] = "Codes",
+    [2] = "Redeem",
+    [3] = {
+        ["Code"] = "FATSTACKZ"
+    }
+}
+
+game:GetService("ReplicatedStorage").Replicator:InvokeServer(unpack(args))
+
+local args = {
+    [1] = "Codes",
+    [2] = "Redeem",
+    [3] = {
+        ["Code"] = "ITJUSTDONTSTOP"
+    }
+}
+
+game:GetService("ReplicatedStorage").Replicator:InvokeServer(unpack(args))
+
+local args = {
+    [1] = "Codes",
+    [2] = "Redeem",
+    [3] = {
+        ["Code"] = "RAININGGEMS!"
+    }
+}
+
+game:GetService("ReplicatedStorage").Replicator:InvokeServer(unpack(args))
+
+local args = {
+    [1] = "Codes",
+    [2] = "Redeem",
+    [3] = {
+        ["Code"] = "GEARFOOOOURTH"
+    }
+}
+
+game:GetService("ReplicatedStorage").Replicator:InvokeServer(unpack(args))
+
+local args = {
+    [1] = "Codes",
+    [2] = "Redeem",
+    [3] = {
+        ["Code"] = "LETSGOO130K"
+    }
+}
+
+game:GetService("ReplicatedStorage").Replicator:InvokeServer(unpack(args))
+
+local args = {
+    [1] = "Codes",
+    [2] = "Redeem",
+    [3] = {
+        ["Code"] = "GOLDENDAYZ"
+    }
+}
+
+game:GetService("ReplicatedStorage").Replicator:InvokeServer(unpack(args))
